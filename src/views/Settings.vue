edc35fe22e3b4f81d82b5de48302aaf0d40aad6a
Shohei Kodama <62084913+sho246@users.noreply.github.com>
2021-08-12 22:11:19 +0900

    Update Settings.vue

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -30,14 +30,14 @@
       <v-text-field
         label="予算カテゴリ"
         v-model="settings.strIncomeItems"
-        :counter="150"
+        :counter="250"
         :rules="[stringRule, ...categoryRules]"
       />
       <!-- 支出カテゴリ -->
       <v-text-field
         label="支出カテゴリ"
         v-model="settings.strOutgoItems"
-        :counter="150"
+        :counter="250"
         :rules="[stringRule, ...categoryRules]"
       />
       <!-- タグ -->
